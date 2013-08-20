93b2425248013f516f941fb47c024acc6973215b
ronsavage <ron@savage.net.au>
2013-08-21 09:06:40 +1000

    Clean up scripts/bnf2graph.sh

--- scripts/bnf2graph.sh
+++ scripts/bnf2graph.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# Name: g2g.sh.
+# Name: bnf2graph.sh.
 #
 # Parameters:
 # 1: The abbreviated name of sample input and output data files.
 #	E.g. xyz simultaneously means data/xyz.bnf, data/xyz.log and html/xyz.svg.
-# 2 & 3: Use for debugging. E.g.: -maxlevel debug.
+# 2 .. 5: Use for debugging. E.g.: -maxlevel debug.
 
-perl -Ilib scripts/g2g.pl -i data/$1.bnf -o html/$1.svg -t data/$1.log $2 $3
+perl -Ilib scripts/bnf2graph.pl -marpa share/metag.bnf -o html/$1.svg -user share/$1.bnf $2 $3 $4 $5
