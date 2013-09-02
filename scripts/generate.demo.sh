579f67a4fbd3912f15218b9fde6bb1f3cc97edec
ronsavage <ron@savage.net.au>
2013-09-02 10:39:59 +1000

    Ensure demo images have legends

--- scripts/generate.demo.sh
+++ scripts/generate.demo.sh
@@ -2,13 +2,13 @@
 
 if [ -n "$1" ]; then
 	echo Processing c.ast. Wait 6 m 47 secs
-	scripts/bnf2graph.sh c.ast
+	scripts/bnf2graph.sh c.ast -legend 1
 fi
 
-scripts/bnf2graph.sh json.1
-scripts/bnf2graph.sh json.2
-scripts/bnf2graph.sh stringparser
-scripts/bnf2graph.sh termcap.info
+scripts/bnf2graph.sh json.1 -legend 1
+scripts/bnf2graph.sh json.2 -legend 1
+scripts/bnf2graph.sh stringparser -legend 1
+scripts/bnf2graph.sh termcap.info -legend 1
 
 perl -Ilib scripts/generate.demo.pl
 
